f4de8d6f1fd1eff626ebe98e1ff69c2c8bd554b3
Dannylui58 <143028255+Dannylui58@users.noreply.github.com>
2024-04-03 12:25:39 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,11 +11,9 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     int count = 0;
-    for(int i = 0;i<myList.size();i++){
-      if(myList.get(i).length()==len){
+    for(int i = 0;i<myList.size();i++)
+      if(myList.get(i).length()==len)
         count++;
-  }
-  }
     return count;
     
   }
@@ -27,7 +25,7 @@ public class WordList
       if(myList.get(i).length()==len)
         myList.remove(i);
       else
-        i--;
+        i++;
   }
     return myList;
   }
